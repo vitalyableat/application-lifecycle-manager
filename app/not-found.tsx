eeb8d7684fc11f32f81f5c0370abb0460f9c8d91
Vitaly Olhov <olivertears@gmail.com>
2023-10-18 16:28:50 +0200

    style: not found page styles refactor

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
 
 const NotFoundPage: FC = () => {
-  return (
-    <div className="flex flex-col w-full h-[calc(100vh-64px)] justify-center text-center">
-      <p className="text-3xl font-bold w-full">Page not found :(</p>
-    </div>
-  );
+  return <div className="flex w-full h-full justify-center items-center text-3xl font-bold">Page not found :(</div>;
 };
 
 export default NotFoundPage;
