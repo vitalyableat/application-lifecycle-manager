eef7714b4bc01b02d69bba989529c10b5d504c2c
Vitaly Olhov <olivertears@gmail.com>
2023-12-13 21:36:34 +0100

    feat: main page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,5 +1,20 @@
+'use client';
+import useAuthStore from '@/services/auth';
+
+const GREETINGS = ['Good night', 'Good morning', 'Good afternoon', 'Good evening'];
+
 const HomePage = () => {
-  return <main>Application Lifecycle Manager</main>;
+  const user = useAuthStore((state) => state.user);
+  const hours = new Date().getHours();
+  const greeting = GREETINGS[+(hours / 6).toString()[0]];
+
+  return (
+    user && (
+      <main className="font-bold text-3xl h-full w-full flex items-center justify-center">
+        {greeting}, {user?.name} {user?.surname}!
+      </main>
+    )
+  );
 };
 
 export default HomePage;
